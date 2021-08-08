f645b1b11bdbeee99179752bab6c237473751b0b
Mercybett <merjep551@gmail.com>
2021-08-08 16:16:11 +0300

    add largeCard

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import LargeCard from '../components/LargeCard';
 import MediumCard from '../components/MediumCard';
 import SmallCard from '../components/SmallCard';
 
@@ -36,6 +37,12 @@ export default function Home({exploreData, cardsData}) {
             ))}
           </div>
         </section>
+        <LargeCard
+          img=""
+          title="The Greatest Outdoors"
+          description="Wishlist curated by airbnb"
+          buttonText="Get Inspired"
+        />
       </main>
     </div>
   )
