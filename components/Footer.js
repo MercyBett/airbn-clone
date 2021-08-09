a7edc5df938f1292380276c4ce2f52275fcff895
Mercybett <merjep551@gmail.com>
2021-08-09 11:50:03 +0300

    add footer

---
+++ components/Footer.js
@@ -0,0 +1,37 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 space-y-10 px-32 py-14 bg-gray-100">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>How it works</p>
+        <p></p>
+        <p></p>
+        <p></p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Community</h5>
+        <p>Accessibility</p>
+        <p></p>
+        <p></p>
+        <p></p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Host</h5>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p>Join Now</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Support</h5>
+        <p> Help Center</p>
+        <p> Terms & Conditions</p>
+        <p></p>
+        <p></p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { GlobeAltIcon, MenuIcon, SearchIcon, UserCircleIcon } from "@heroicons/react/solid"
 function Header() {
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
@@ -13,15 +14,15 @@ function Header() {
       {/* middle - the search */}
       <div className="flex-grow items-center rounded-full md:border-2 md:shadow-sm py-2">
         <input className="outline-none bg-transparent pl-5 text-gray-400 text-sm" type="text" placeholder="Search" />
-        {/* <SearchIcon className="hidden md:inline-flex md:mx-2 p-2 h-8 bg-red-400 text-white rounded-full cursor-pointer"/> */}
+        <SearchIcon className="hidden md:inline-flex md:mx-2 p-2 h-8 bg-red-400 text-white rounded-full cursor-pointer"/>
       </div>
       {/* right - the profile-ish*/}
       <div className="flex items-center justify-end space-x-4 text-gray-500">
         <p className="hidden md:inline cursor-pointer">Become  a host</p>
-        {/* <GlobeAltIcon className="h6 cursor-pointer"/> */}
+        <GlobeAltIcon className="h6 cursor-pointer"/>
         <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
-          {/* <MenuIcon className="h-6"*/}
-          {/* <UserCircleIcon className="h-6"/> */}
+          <MenuIcon className="h-6"
+          <UserCircleIcon className="h-6"/>
         </div>
       </div>
     </header>
