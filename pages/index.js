dae9d777b6ad1fbc15fecfb0c5da3531e9ee7d57
Mercybett <merjep551@gmail.com>
2021-08-20 13:23:43 +0300

    remove x @smallCard on index

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,39 @@
-import Head from 'next/head'
-import Banner from '../components/Banner'
-import Footer from '../components/Footer';
-import Header from '../components/Header'
-import LargeCard from '../components/LargeCard';
-import MediumCard from '../components/MediumCard';
-import SmallCard from '../components/SmallCard';
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import LargeCard from "../components/LargeCard";
+import MediumCard from "../components/MediumCard";
+import SmallCard from "../components/SmallCard";
 
-
-export default function Home({exploreData, cardsData}) {
+export default function Home({ exploreData, cardsData }) {
   return (
     <div>
       <Head>
         <title>Airbnb Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
-      <Banner/>
+      <Header />
+      <Banner />
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
           {/* pull data from server -API endpoints */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">x
-          {exploreData?.map(item=>(
-            <SmallCard
-            key={item.img}
-            img={item.img}
-            location={item.location}
-            distance={item.distance}
-            />
-          ))}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map((item) => (
+              <SmallCard
+                key={item.img}
+                img={item.img}
+                location={item.location}
+                distance={item.distance}
+              />
+            ))}
           </div>
         </section>
         <section>
           <h2 className="text-4xl text-semibold py-8">Live Anywhere</h2>
           <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
-            {cardsData?.map(item =>(
+            {cardsData?.map((item) => (
               <MediumCard key={item.img} img={item.img} title={item.title} />
             ))}
           </div>
@@ -46,21 +45,21 @@ export default function Home({exploreData, cardsData}) {
           buttonText="Get Inspired"
         />
       </main>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
-export async function getStaticProps(){
+export async function getStaticProps() {
   const exploreData = await fetch("https://links.papareact.com/pyp").then(
     (res) => res.json()
   );
-  const cardsData = await fetch("https://links.papareact.com/zp1").then(
-    (res) => res.json()
+  const cardsData = await fetch("https://links.papareact.com/zp1").then((res) =>
+    res.json()
   );
   return {
-    props:{
+    props: {
       exploreData,
       cardsData,
-    }
-  }
+    },
+  };
 }
